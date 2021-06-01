5d482a2dfa12bce3b245305324a7ae198f9b268d
Allie <alliecaton3@gmail.com>
2021-06-01 12:23:57 -0400

    installing boostrap for nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className="navbar-parent">
+                <a>about </a>  
+                <a>projects </a>  
+                <a>contact</a>
+        </div>
+    );
+}
+
+export default NavBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from '../components/NavBar'
 
 class MainContainer extends Component {
     render() {
         return (
-            <div>
+            <div className="wrapper">
+                <NavBar />
                 main container
             </div>
         );
