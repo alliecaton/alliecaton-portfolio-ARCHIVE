93110e595c1be15f0191528c7327890cadbcdf3e
Allie <alliecaton3@gmail.com>
2021-06-02 17:29:39 -0400

    styling on about component

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -4,10 +4,19 @@ import WorkExperience from '../components/about/WorkExperience'
 
 const AboutContainer = ({ id }) => {
     return (
-        <div className="about-container" id={id}>
-            <Description />
-            <WorkExperience />
+        <div className="about-container-top">
+                <p><span className="dark-purple big">class</span>  
+                <span className="yellow big"> About</span>  
+                <span className="dark-purple big"> {`{`}</span> </p>
+            <div className="vertical-line">
+                <div className="about-container" id={id}>
+                    <Description />
+                    <WorkExperience />
+                </div>
+            </div>
+            <p className="dark-purple big"> {`}`} </p>
         </div>
+        
     );
 }
 
