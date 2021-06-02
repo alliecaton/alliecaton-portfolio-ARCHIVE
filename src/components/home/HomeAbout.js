2aadd0eaefc0a05fbdeba7d05adaac577d899b73
Allie <alliecaton3@gmail.com>
2021-06-02 13:53:51 -0400

    vertical lines

--- src/components/home/HomeAbout.js
+++ src/components/home/HomeAbout.js
@@ -7,10 +7,12 @@ const HomeAbout = () => {
             <span className="yellow big"> allieCaton</span>  
             <span className="dark-purple big"> {`{`}</span> </p>
             <div className="indented">
-                <p className="yellow ">full-stack developer</p>
-                <p className="yellow ">javascript</p>
-                <p className="yellow ">react + redux</p>
-                <p className="yellow">ruby + ruby on rails</p>
+                <div className="indiv-indented">
+                    <p className="yellow">full-stack developer</p>
+                    <p className="yellow">javascript</p>
+                    <p className="yellow">react + redux</p>
+                    <p className="yellow">ruby + ruby on rails</p>
+                </div>
             </div>
             <p className="dark-purple big"> {`}`} </p>
         </div>
