3b10dce56630dd73f0258ad3341151c612b083f2
Allie <alliecaton3@gmail.com>
2021-06-03 17:09:42 -0400

    setup state for contact form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 
 class ContactForm extends Component {
+
+    state = {
+        name: '', 
+        email: '',
+        subject: '', 
+        message: ''
+    }
+
+    handleChange = (e) => {
+        this.setState(prevState => ({
+                ...prevState, 
+                [e.target.name]: e.target.value, 
+        }))
+    }
+
     render() {
+        console.log(this.state)
         return (
             <div className="top-form-flex">
                 <form className="form-flex">
                     <label className="teal">name:</label>
-                    <input type="text" />
+                    <input type="text" name="name" onChange={this.handleChange} />
 
                     <label className="teal">email:</label>
-                    <input type="text" />
+                    <input type="text" name="email" onChange={this.handleChange} />
 
                     <label className="teal">subject:</label>
-                    <input type="text" />
+                    <input type="text" name="subject" onChange={this.handleChange} />
 
-                    <label className="teal" for="message">message:</label>
-                    <textarea id="message"></textarea>
+                    <label className="teal" htmlFor="message">message:</label>
+                    <textarea id="message" name="message" onChange={this.handleChange}></textarea>
+
+                    <input type="submit" value="Send Message" />
                 </form>
             </div>
         );
