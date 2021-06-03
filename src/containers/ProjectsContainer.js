2faa63363796f49fe05b16ae74b326f5f4b3819e
Allie <alliecaton3@gmail.com>
2021-06-03 10:19:22 -0400

    projects container setup

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const ProjectsContainer = ({ id }) => {
     return (
-        <div id={id}>
-            projects
+        <div className="about-container-top">
+                <p><span className="dark-purple big">class</span>  
+                <span className="yellow big"> Projects</span>  
+                <span className="dark-purple big"> {`{`}</span> </p>
+            <div className="vertical-line">
+                <div className="about-container" id={id}>
+                    projects
+                </div>
+            </div>
+            <p className="dark-purple big"> {`}`} </p>
         </div>
     );
 }
