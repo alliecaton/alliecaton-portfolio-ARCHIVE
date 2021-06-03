5b57358868ee4729b72016ad0cf2475d05711a10
Allie <alliecaton3@gmail.com>
2021-06-03 16:13:42 -0400

    updated gh and demo project links

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -4,22 +4,22 @@ export const projectData = [
         id: 1,
         title: "Fruit Stand", 
         description: "Demo e-commerce site for a smoothie shop with custom smoothie-builder and cart/checkout functionality.",
-        github: "www.github.com", 
-        demo: "www.demo.com"    
+        github: "https://github.com/alliecaton/smoothie-shop-frontend", 
+        demo: "https://alliecaton.github.io/smoothie-shop-frontend/"    
     }, 
     {
         id: 2,
         title: "music-r", 
         description: "SPA for finding and saving curated song recommendations. ",
-        github: "www.github.com", 
-        demo: "www.demo.com"  
+        github: "https://github.com/alliecaton/jsmusicrandomizer", 
+        demo: "plausible-nest.surge.sh/"  
     }, 
     {
         id: 3,
         title: "Shelvd", 
         description: "Book tracking application with functionality for organizing books as well as a chat feature for connecting with other users. ",
-        github: "www.github.com", 
-        demo: "www.demo.com" 
+        github: "https://github.com/alliecaton/shelvd", 
+        demo: "https://shelvd-app.herokuapp.com/" 
     }, 
 
 ]
