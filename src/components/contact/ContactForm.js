c477abcba4763ceb8865a00b4ebc53f15c039670
Allie <alliecaton3@gmail.com>
2021-06-03 22:33:31 -0400

    clear contact form after submit

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -23,28 +23,29 @@ class ContactForm extends Component {
         emailjs.sendForm('service_36eqziu', 'template_mb1ober', e.target, 'user_k8P1ZIwiNW2urIBeetEiU')
         .then((result) => {
             console.log(result.text);
+            this.setState({from_name: '', email: '', subject: '', message: ''})
         }, (error) => {
             console.log(error.text);
         });
 
-
     }
 
     render() {
+        console.log(this.state)
         return (
             <div className="top-form-flex">
                 <form className="form-flex" onSubmit={this.handleSubmit}>
                     <label className="teal">name:</label>
-                    <input type="text" name="from_name" onChange={this.handleChange} />
+                    <input type="text" value={this.state.from_name} name="from_name" onChange={this.handleChange} />
 
                     <label className="teal">email:</label>
-                    <input type="text" name="email" onChange={this.handleChange} />
+                    <input type="text" value={this.state.email} name="email" onChange={this.handleChange} />
 
                     <label className="teal">subject:</label>
-                    <input type="text" name="subject" onChange={this.handleChange} />
+                    <input type="text" value={this.state.subject} name="subject" onChange={this.handleChange} />
 
                     <label className="teal" htmlFor="message">message:</label>
-                    <textarea id="message" name="message" onChange={this.handleChange}></textarea>
+                    <textarea id="message" value={this.state.message} name="message" onChange={this.handleChange}></textarea>
 
                     <input type="submit" value="Send!" />
                 </form>
