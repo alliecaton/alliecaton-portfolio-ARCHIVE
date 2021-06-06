5d70820d702111532756b1dec46fe558d50450e2
Allie <alliecaton3@gmail.com>
2021-06-06 16:32:27 -0400

    adding tech

--- src/components/about/Description.js
+++ src/components/about/Description.js
@@ -8,7 +8,15 @@ const Description = () => {
             <span className="dark-purple big"> {`{`}</span> </p>
             <div className="indented">
                 <div className="indiv-indented">
-                    <p className="yellow">I'm Allie _________. I'm interested in building simple, clean, effective websites ad applications for users. </p>
+                    <p className="yellow">I'm Allie, a full-stack web developer interested in building simple, clean, effective websites and applications with people who are similarly passionate. ___ </p>
+                    <p className="yellow">In my free time, I dabble in building electronic hardware, drawing, reading, and mooning over my mechanical keyboards. 
+                    </p>
+                    <p className="yellow">Here are a few technologies I've been working with recently:</p>
+                    <ul>
+                        <p className="teal"> <span className="light-purple">></span> Javascript (ES6+)</p>
+                        <p className="teal"><span className="light-purple">></span>  Ruby + Ruby on Rails</p>
+                        <p className="teal"><span className="light-purple">></span> React</p>
+                    </ul>
                 </div>
             </div>
             <p className="dark-purple big"> {`}`} </p>
