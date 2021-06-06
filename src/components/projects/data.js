141f88d3ffc23393e77b83fecf1909c69941e724
Allie <alliecaton3@gmail.com>
2021-06-06 15:26:03 -0400

    updated data

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -4,7 +4,7 @@ export const projectData = [
         id: 1,
         title: "Fruit Stand", 
         description: "Demo e-commerce site for a smoothie shop with custom smoothie-builder and cart/checkout functionality.",
-        tech: "React • Redux • Ruby on rails",
+        tech: "React • Redux • Ruby on Rails • ActiveRecord",
         github: "https://github.com/alliecaton/smoothie-shop-frontend", 
         demo: "https://alliecaton.github.io/smoothie-shop-frontend/"    
     }, 
@@ -12,7 +12,7 @@ export const projectData = [
         id: 2,
         title: "music-r", 
         description: "SPA for finding and saving curated song recommendations. ",
-        tech: "Vanilla Javascript • Ruby on Rails",
+        tech: "Vanilla Javascript • Ruby on Rails • ActiveRecord",
         github: "https://github.com/alliecaton/jsmusicrandomizer", 
         demo: "http://plausible-nest.surge.sh/"  
     }, 
@@ -20,7 +20,7 @@ export const projectData = [
         id: 3,
         title: "Shelvd", 
         description: "Book tracking application with functionality for organizing books as well as a chat feature for connecting with other users. ",
-        tech: "",
+        tech: "Ruby on Rails • ActiveRecord",
         github: "https://github.com/alliecaton/shelvd", 
         demo: "https://shelvd-app.herokuapp.com/" 
     }, 
